cf98d32df5b3e1ba585243f72143e179bf2ed54d
Bruce Wayne <theknight@Bruces-MacBook-Air.local>
2024-10-25 21:19:38 +0530

    Files added

---
+++ main.js
@@ -0,0 +1,7 @@
+// Create different sudoku boards 
+    //with a title 
+// Give option to choose from 3-6 boards (if possible 9 with every 3 increasing in difficulty)
+    //give a button to solve the board
+    //also to reset the board
+// create a backtracking algo in js to solve the sudoku board
+// give the animation for random numbers changing in the sudoku board
